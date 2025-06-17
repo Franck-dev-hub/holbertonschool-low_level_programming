6db1ec99b35d6cb7938729e19679fff2050bbf0f
Franck-dev-hub <franck.spadotto@yahoo.com>
2025-06-17 10:33:46 +0200

    Update 2-print_alphabet.c that prints the alphabet in lowercase, followed by a new line.

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,18 @@
 #include <time.h>
 
 /**
- * main - Infos here
+ * main - This program will assign a random number
+ *        to the variable n each time it is executed.
+ *
+ * The variable n will store a different valuee
+ * every time you will run this program
+ * The output of the program should be:
+ *   The number, followed by
+ *     if the number is greater than 0: is positive
+ *     if the number is 0: is zero
+ *     if the number is less than 0: is negative
+ *   followed by a new line
+ *
  * Return: 0
  */
 
---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Write a program that prints the alphabet in lowercase,
+ *        followed by a new line
+ *
+ * You can only use the putchar function
+ * (every other function (printf, puts, etc…) is forbidden)
+ * All your code should be in the main function
+ * You can only use putchar twice in your code
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+char c = 'a';
+
+for (; c <= 'z'; c++)
+putchar(c);
+
+putchar('\n');
+return (0);
+}
