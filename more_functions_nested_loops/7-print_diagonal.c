0460bde5bfb7d0858f67c0b30eeaea378be63f78
Franck-dev-hub <franck.spadotto@yahoo.com>
2025-06-23 10:52:23 +0200

    Update 7-print_diagonal.c file
    Print a diagonal of n length

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Bi
+ * @n: n for a number
+ *
+ * Return: 0
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0 ; i < n ; i++)
+		{
+			for (j = 0 ; j < n ; j++)
+			{
+				if (i == j)
+				{
+					_putchar('\\');
+					break;
+				}
+				else
+			{
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
