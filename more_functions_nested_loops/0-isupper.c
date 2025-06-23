24834411b225154fd3d8206965c03b5c1f787471
Franck-dev-hub <franck.spadotto@yahoo.com>
2025-06-23 09:24:02 +0200

    Create all project files + update 0-isupper.c file
    0-isupper.c is a function that checks for uppercase character.

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - Upper case ... check
+ * @c: c c c c
+ *
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+	if (c > 64 || c < 91)
+		retrun (1);
+	else
+		return (0);
+}
