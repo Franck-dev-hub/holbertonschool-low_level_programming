ea1d78d0b83560e10c3a19c527056658fac464b7
Franck-dev-hub <franck.spadotto@yahoo.com>
2025-06-23 09:39:14 +0200

    Update 1-isdigit.c file
    It check if it's a number or anything else

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - Digit ... check
+ * @c: ## == Description here == ##
+ *
+ * Return: 0
+ */
+
+int _isdigit(int c)
+{
+	if (c > 47 && c < 58)
+		return (1);
+	else
+		return (0);
+}
+
