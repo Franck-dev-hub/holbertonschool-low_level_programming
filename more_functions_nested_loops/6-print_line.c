9a277aeece731965325bd06f29e88c8c89f803ae
Franck-dev-hub <franck.spadotto@yahoo.com>
2025-06-23 10:35:44 +0200

    Update 6-print_line.c file
    It print a straight line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - Not gay
+ * @n: n for a number
+ *
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	for (; n > 0; n--)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
