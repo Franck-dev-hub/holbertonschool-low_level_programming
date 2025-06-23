d412f7b15d608e260b93ad913749d40bc8cf44af
Franck-dev-hub <franck.spadotto@yahoo.com>
2025-06-23 14:17:30 +0200

    Create all project files + update 0-reset_to_98.c file
    Update the pointer value to 98

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,43 +1,24 @@
 #include "main.h"
 
 /**
- * print_number - Prints any integer with putchar only
- * @n: Integer
+ * print_number - The basic of all integers
+ * @n: integer to integrate
+ * Return: 0
  */
+
 void print_number(int n)
 {
-	int length, digit, power;
-	int i, j, x = 1;
+	unsigned int num;
 
 	if (n < 0)
 	{
-		_putchar(45);
-		n = -n;
+		num = -n;
+		_putchar('-');
 	}
-
-	/* Getting the number of digits of n */
-	if (n == 0)
-		length = 1;
 	else
-	{
-		length = 0;
-		while (n / x != 0)
-		{
-			x *= 10;
-			length++;
-		}
-	}
+	num = n;
 
-	/* Printing each digit of n */
-	for (i = length ; i > 0 ; i--)
-	{
-		power = 1;
-		for (j = 0 ; j < i - 1 ; j++)
-			power *= 10;
-		if (n % (power * 10) >= power)
-			digit = (n / power) % 10;
-		else
-			digit = 0;
-		_putchar('0' + digit);
-	}
+	if (num / 10)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
 }
---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Back to 90's
+ * @n: no no no no no
+ *
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
