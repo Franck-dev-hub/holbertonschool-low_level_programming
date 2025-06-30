dbaa6af1c58d1d5245225b5a91665eea48c7e158
Franck-dev-hub <franck.spadotto@yahoo.com>
2025-06-30 10:22:27 +0200

    Update 0-memset.c comments

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 /**
- * *_strcat - Like a cat
- * @dest: Cat basket
+ * *_memset - fills memory with a constant byte
+ * @s: buffer to fill
+ * @b: constant byte to fill with
+ * @n: number of bytes to fill
  *
  * Return: 0
  */
