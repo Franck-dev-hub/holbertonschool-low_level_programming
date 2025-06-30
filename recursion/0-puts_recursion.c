7c337a5dc23053bb06eff549219ec2be2cc611a4
Franck-dev-hub <franck.spadotto@yahoo.com>
2025-06-30 17:04:24 +0200

    Update 0-puts_recursion files
    It print a string without a loop function

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string input
+ *
+ * Return: 0
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		_puts_recursion(&s[i + 1]);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
