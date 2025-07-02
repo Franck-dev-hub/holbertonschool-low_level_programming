ed9c56eeb7eb1c7424659c6b6c1b95c0736239b3
Franck-dev-hub <franck.spadotto@yahoo.com>
2025-07-02 17:23:40 +0200

    Update 0-whatsmyname.c file + del main.h
    main.h seams useless for the project

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: count
+ * @argv: vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
