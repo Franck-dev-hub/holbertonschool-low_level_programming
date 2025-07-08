e623920f0cacf75c6c036c91ead63eb8d3c352df
Franck-dev-hub <franck.spadotto@yahoo.com>
2025-07-08 09:25:49 +0200

    Update dog.h file
    It's a structure about dogs

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure to store informations about dogs
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
