3b634f876b56c93ed4f1d9634087926340bea95b
Franck-dev-hub <franck.spadotto@yahoo.com>
2025-07-09 18:17:52 +0200

    Add if not a number verification

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <ctype.h>
 
 /**
  * main - multiplies two positive numbers
@@ -11,18 +12,24 @@
 
 int main(int argc, char *argv[])
 {
-	int mul = 0, num1 = 0, num2 = 0;
+	int i;
+	unsigned long int mul = 0, num1 = 0, num2 = 0;
 
-	if (argc != 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
+	if (argc != 3 || !*argv[1] || !*argv[2])
+		return (printf("Error\n"), exit(98), 98);
+
+	for (i = 0; argv[1][i]; i++)
+		if (!isdigit(argv[1][i]))
+			return (printf("Error\n"), exit(98), 98);
+
+	for (i = 0; argv[2][i]; i++)
+		if (!isdigit(argv[2][i]))
+			return (printf("Error\n"), exit(98), 98);
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
 
 	mul = num1 * num2;
-	printf("%i\n", mul);
+	printf("%li\n", mul);
 	return (0);
 }
