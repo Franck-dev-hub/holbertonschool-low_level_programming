d69715973c576036dcd469549eaa5fd22e0cab6c
Franck-dev-hub <franck.spadotto@yahoo.com>
2025-07-14 18:37:42 +0200

    Update logic (Checker said I have 0/9 ...)

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -19,26 +19,31 @@ void print_all(const char * const format, ...)
 
 	while (format && format[i])
 	{
-		if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+		if (format[i] == 'c')
 		{
-			printf("%s", sep);
-			if (format[i] == 'c')
-				printf("%c", va_arg(args, int));
-			if (format[i] == 'i')
-				printf("%i", va_arg(args, int));
-			if (format[i] == 'f')
-				printf("%f", va_arg(args, double));
-			if (format[i] == 's')
-			{
-				str = va_arg(args, char *);
-				if (str == NULL)
-					str = "(nil)";
-				printf("%s", str);
-			}
+			printf("%s%c", sep, va_arg(args, int));
+			sep = ", ";
+		}
+		else if (format[i] == 'i')
+		{
+			printf("%s%d", sep, va_arg(args, int));
+			sep = ", ";
+		}
+		else if (format[i] == 'f')
+		{
+			printf("%s%f", sep, va_arg(args, double));
+			sep = ", ";
+		}
+		else if (format[i] == 's')
+		{
+			str = va_arg(args, char *);
+			if (!str)
+				str = "(nil)";
+			printf("%s%s", sep, str);
 			sep = ", ";
 		}
 		i++;
 	}
-	printf("\n");
 	va_end(args);
+	printf("\n");
 }
