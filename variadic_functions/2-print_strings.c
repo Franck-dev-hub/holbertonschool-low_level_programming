60f946398d7780db642f3dd79d727add1a100de4
Franck-dev-hub <franck.spadotto@yahoo.com>
2025-07-14 17:20:17 +0200

    Update 2-print_strings.c file
    It prints strings separate by a string

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * print_strings - print strings
+ * @separator: string to be printed between
+ * @n: strings
+ *
+ * Return: 0
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+	char *str;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char *);
+
+		if (separator != NULL && str != NULL)
+			printf("%s", str);
+		if (i != n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
+
