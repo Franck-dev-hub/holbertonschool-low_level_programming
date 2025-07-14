936f8126e26fda1643de784c98e61fac5139a1b3
Franck-dev-hub <franck.spadotto@yahoo.com>
2025-07-14 09:47:13 +0200

    Update 0-print_name.c file
    It prints a name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: the name
+ * @f: function
+ *
+ * Return: 0
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL)
+	{
+		f(name);
+	}
+}
