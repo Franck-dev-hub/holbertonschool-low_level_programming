f665eecec88d5bdb121e374d7ab1422a9ad5c98c
Franck-dev-hub <franck.spadotto@yahoo.com>
2025-07-14 16:55:49 +0200

    Update 1-print_numbers.c file
    It prints numbers separate by a string

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - print numbers
+ * @separator: string to be printed between
+ * @n: numbers
+ *
+ * Return: 0
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (separator != NULL)
+			printf("%i", va_arg(args, int));
+		if (i != n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
