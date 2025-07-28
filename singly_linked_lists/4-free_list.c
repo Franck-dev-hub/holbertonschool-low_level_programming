05672be0fafb709da1ca3462310353a3cfc87c9c
Franck <franck.spadotto@yahoo.com>
2025-07-28 18:24:51 +0200

    Update 4-free_list.c
    It free list memory

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_list - free linked list
+ * @head: head
+ *
+ * Return: 0
+ */
+
+void free_list(list_t *head)
+{
+
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
+
