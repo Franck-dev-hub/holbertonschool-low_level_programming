a846a55a044dfc13520912a5c6beeaa599a92ff4
Franck <franck.spadotto@yahoo.com>
2025-07-31 22:34:03 +0200

    Add print_error function

--- file_io/main.h
+++ file_io/main.h
@@ -14,5 +14,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int copy_text_to_file(const char *file_from, const char *file_to);
+void print_error(const char *msg, const char *arg, int code);
 
 #endif
