c11e8eb2f878d73d5522e647934fbfbf6c785d53
Franck <franck.spadotto@yahoo.com>
2025-07-31 21:09:57 +0200

    Remove mod flag (useless whitout O_CREAT)

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -14,7 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char*buffer;
 	ssize_t bytes_read, bytes_written;
 
-	file = open(filename, O_RDONLY, 0444);
+	file = open(filename, O_RDONLY);
 	if (file == -1)
 		return (0);
 
