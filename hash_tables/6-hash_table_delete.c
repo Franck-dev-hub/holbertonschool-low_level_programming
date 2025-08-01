ead0374e2216ef400b69cb601220e72ded2906ba
Franck <franck.spadotto@yahoo.com>
2025-08-01 17:41:32 +0200

    Update 6-hash_table_delete.c
    Empty the hash table.

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Delete the hash table.
+ * @ht: Hash table.
+ *
+ * Return: Void
+ */
+
+
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node, *temp;
+
+	if (!ht)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			temp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = temp;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
